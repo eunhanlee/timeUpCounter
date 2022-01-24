31fa201534365e94e1aefdca7072be8a4d82ff0f
EH <eunhanlee58@gmail.com>
2022-01-24 17:20:57 -0500

    start

---
+++ main.py
@@ -0,0 +1,122 @@
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget
+from PyQt5.QtWidgets import QGraphicsOpacityEffect
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QFrame, QSplitter
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QDateTime, Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import QCoreApplication
+from threading import Timer,Thread,Event
+
+from perpetualTimer import perpetualTimer
+
+
+class MyApp(QWidget):
+    global count
+    count=0
+
+
+    def __init__(self):
+
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        global t
+        t = perpetualTimer(self.changeLabel)
+
+
+
+        btn1 = QPushButton(self)
+        btn1.setText("Start")
+        btn1.clicked.connect(self.start)
+
+        btn2 = QPushButton(self)
+        btn2.setText("pause")
+        btn2.clicked.connect(self.pause)
+        btn2.setDisabled(True)
+
+        btn3 = QPushButton(self)
+        btn3.setText("stop")
+        btn3.clicked.connect(self.stop)
+
+        btn4 = QPushButton(self)
+        btn4.setText("Hold")
+        btn4.clicked.connect(self.Hold)
+        btn4.setDisabled(True)
+
+        btn5 = QPushButton(self)
+        btn5.setText("exit")
+        btn5.clicked.connect(QCoreApplication.instance().quit)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(btn1)
+        hbox.addWidget(btn2)
+        hbox.addWidget(btn3)
+        hbox.addWidget(btn4)
+        hbox.addWidget(btn5)
+        hbox.addStretch(1)
+
+        self.label1 = QLabel("00:00:00", self)
+        self.label1.setAlignment(Qt.AlignCenter)
+        font1 = self.label1.font()
+        font1.setPointSize(50)
+        font1.setFamily("D2 Coding")
+        font1.setBold(True)
+        self.label1.setFont(font1)
+
+        vbox = QVBoxLayout()
+
+        vbox.addWidget(self.label1)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+        self.setWindowTitle("Time Counter")
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.adjustSize()
+        # self.resize(450, 200)
+        self.center()
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def Hold(self):
+        # self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setWindowFlag(Qt.WindowStaysOnBottomHint)
+        self.show()
+
+    def start(self):
+        # self.changeLabel()
+        t.start()
+
+
+    def pause(self):
+        t.pause()
+
+    def stop(self):
+        t.cancel()
+
+    def changeLabel(self):
+        global count
+        temp = self.label1.text()
+        print(temp)
+        count+=1
+        self.label1.setText(str(count))
+
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+
+    sys.exit(app.exec_())
+
---
+++ perpetualTimer.py
@@ -0,0 +1,20 @@
+from threading import Timer,Thread,Event
+
+
+class perpetualTimer():
+
+   def __init__(self,hFunction):
+      self.t=1
+      self.hFunction = hFunction
+      self.thread = Timer(self.t,self.handle_function)
+
+   def handle_function(self):
+      self.hFunction()
+      self.thread = Timer(self.t,self.handle_function)
+      self.thread.start()
+
+   def start(self):
+      self.thread.start()
+
+   def cancel(self):
+      self.thread.cancel()
